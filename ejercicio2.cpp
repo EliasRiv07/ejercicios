33da791e7e556b9827c8df8096b65972188734a7
EliasRiv07 <00137525@uca.edu.sv>
2025-05-06 19:34:50 -0600

    ejercicio

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int n;
+    int result;
+    cout<<"dame un numero""\n";
+    cin>>n;
+    result=n%2;
+if(result=0){
+    cout<<"su numero es par";
+}
+else {
+    if(result=1){
+    cout<<"su numero es impar";
+    }
+}
+    
+    return 0;
 
+}       
